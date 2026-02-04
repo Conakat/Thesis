00d5182b7ee7f96dbad453740888cf25bbb9f0e4
Kaggi <130651143+Conakat@users.noreply.github.com>
2026-02-04 20:45:17 +0200

    Update util.py

--- util.py
+++ util.py
@@ -1,13 +1,1 @@
-import torch
 
-def save_model(model, filename):
-
-    torch.save(model.state_dict(), filename)
-    print(f"Model saved as {filename}")
-
-def load_model(model, filename):
-
-    model.load_state_dict(torch.load(filename))
-    print(f"Model loaded from {filename}")
-
-    return model
